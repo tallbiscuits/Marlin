713b8e7b83eda47236e9d395bd6bfb444e08264f
tallbiscuits <112773728+tallbiscuits@users.noreply.github.com>
2022-09-04 00:08:02 +0200

    Update Configuration_adv.h

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -2116,7 +2116,7 @@
  * the probe to be unable to reach any points.
  */
 #if PROBE_SELECTED && !IS_KINEMATIC
-  #define PROBING_MARGIN_LEFT 30
+  #define PROBING_MARGIN_LEFT 10
   #define PROBING_MARGIN_RIGHT 30
   #define PROBING_MARGIN_FRONT 30
   #define PROBING_MARGIN_BACK 30
